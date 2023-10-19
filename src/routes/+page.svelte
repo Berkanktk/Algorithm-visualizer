8aa48f374c7e3f3683ec25fe668bb8e5c7dfb3b4
Berkanktk <berkank@live.dk>
2023-10-19 03:31:25 +0200

    Adde temporary redirect

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<div class="flex flex-col mx-auto items-center justify-center h-screen">
+    <a href="/sorting">Sorting</a>
+    <a href="/searching">Searching</a>
+    <a href="/pathfinding">Pathfinding</a>
+    <a href="/data-structures">Data Structures</a>
+    <a href="/recommendation">Recommendation</a>
+</div>
