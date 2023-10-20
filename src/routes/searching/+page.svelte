11e18272a9cb6da2b878340af0638f4312f5b807
Berkanktk <berkank@live.dk>
2023-10-20 14:57:03 +0200

    Finished search algorithms

--- src/routes/searching/+page.svelte
+++ src/routes/searching/+page.svelte
@@ -3,13 +3,57 @@
 
     let data = ["Apple" , "Banana" , "Cherry" , "Durian" , "Elderberry" , "Fig" , "Grape" , "Honeydew" , "Ice Cream Bean" , "Jackfruit" , "Kiwi" , "Lemon" , "Mango" , "Nectarine" , "Orange" , "Papaya" , "Quince" , "Raspberry" , "Strawberry" , "Tangerine" , "Ugli Fruit" , "Vanilla Bean" , "Watermelon" , "Xigua" , "Yuzu" , "Zucchini"];
 
+    let selectedSearch: any = [
+        {
+            name: "Linear Search",
+            timeComplexity: "O(n)",
+            id: 1
+        },
+        {
+            name: "Binary Search",
+            timeComplexity: "O(log n)",
+            id: 2
+        },
+        {
+            name: "Jump Search",
+            timeComplexity: "O(√n)",
+            id: 3
+        },
+        {
+            name: "Exponential Search",
+            timeComplexity: "O(log n)",
+            id: 4
+        }
+    ];
+
     let textArea: any;
     let searchTerm: any;
-    let selectedSearch: any = ["Linear Search", "Binary Search", "Jump Search","Exponential Search"];
-    let selected: any = "Linear Search";
     let currentStep: any;
-    let speed = 400;
     let visited: any = [];
+    let speed = 400;
+    let selected: any = "Linear Search";
+    let timeComplexity: string = ""; 
+    let totaltJumps: number = visited.length;
+    let isRunning: boolean = false;
+
+    $: selectedSearch; // This reactive statement will track changes to selectedSearch
+    $: {
+        // Update the time complexity whenever selected or selectedSearch changes
+        updateTimeComplexity();
+    }
+
+    $: {
+        // Update the totaltJumps whenever visited changes
+        totaltJumps = visited.length;
+    }
+
+    // A function to update the time complexity based on the selected algorithm
+    function updateTimeComplexity() {
+        const selectedAlgorithm = selectedSearch.find((item: any) => item.name === selected);
+        if (selectedAlgorithm) {
+            timeComplexity = selectedAlgorithm.timeComplexity;
+        }
+    }
 
     function addToList() {
         data = [...data, textArea];
@@ -27,6 +71,11 @@
     }
 
     function search() {
+        isRunning = true;
+        visited = [];
+        currentStep = -1;
+        totaltJumps = 0;
+
         if(selected == "Linear Search") {
             linearSort(searchTerm);
         } else if(selected == "Binary Search") {
@@ -135,21 +184,14 @@
     }
 </script>
 
-<h1 class="text-5xl font-bold mb-12 text-center mt-6">Searching Algorithms</h1>
-
-<div class="flex flex-col mx-auto gap-y-8 items-center h-screen justify-center absolute">
-
-    <div  class="flex space-x-2">
-        <input type="text" class="p-2 rounded-md border w-96" bind:value={textArea} placeholder="Enter item" />
-        
-        <div class="flex space-x-2">
-            <button class="p-2 btn btn-primary rounded-md" on:click={addToList}>Add</button>
-            <button class="p-2 btn btn-primary rounded-md" on:click={() => data = []}>Clear</button>
-            <button class="p-2 btn btn-secondary rounded-md" on:click={removeFromList}>Remove</button>
-        </div>
+<div class="flex flex-col mx-auto gap-y-8 items-center h-screen justify-center relative">
+    <div class="absolute top-0 left-0 right-0 text-center">
+        <h1 class="text-5xl font-bold text-center mt-6">Searching Algorithms</h1>
+        <h2 class="text-3xl font-bold mb-4 text-center mt-16">Time Complexity:</h2>
+        <span class="text-red-600 text-4xl font-bold text-center">{timeComplexity}</span>
     </div>
     
-    <div class="flex flex-col items-center justify-center w-1/2">
+    <div class="flex flex-col w-1/2">
         <div class="flex flex-wrap justify-center">
             {#each data as item, i (i)}
                 <div class="bg-gray-600 p-2 m-1 rounded-md" style="background-color: {currentStep == i ? 'green' : visited.includes(i) ? 'green' : ''};">{item}</div>
@@ -157,17 +199,30 @@
         </div>
     </div>
 
+    <div  class="flex space-x-2 ">
+        <input type="text" class="p-2 rounded-md border w-80" bind:value={textArea} placeholder="Enter item" />
+        
+        <div class="flex space-x-2">
+            <button class="p-2 btn btn-primary rounded-md" on:click={addToList}>Add</button>
+            <button class="p-2 btn btn-primary rounded-md" on:click={() => data = []}>Clear</button>
+            <button class="p-2 btn btn-secondary rounded-md" on:click={removeFromList}>Remove</button>
+        </div>
+    </div>
+
     <div class="flex space-x-2">
         <input type="text" class="p-2 rounded-md border " bind:value={searchTerm} placeholder="Search Term" />
 
-        <select class="p-2 rounded-md border" bind:value={selected}>
+        <select class="p-2 rounded-md border" bind:value={selected} on:change={updateTimeComplexity} >
             {#each selectedSearch as item, i (i)}
-            <option value={item}>{item}</option>
+                <option value={item.name}>{item.name}</option>
             {/each}
         </select>
         
         <button class="p-2 btn btn-primary rounded-md" on:click={search}>Search</button>
         <button class="p-2 btn btn-secondary rounded-md" on:click={() => location.reload()}>Reset</button>
+
     </div>
+    
+    <span>Total Steps: <span style="color: {!isRunning ? 'red' : ''};">{isRunning ? totaltJumps : 'Not running'}</span></span>
 
 </div>
