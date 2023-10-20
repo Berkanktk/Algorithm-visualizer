742c863c5dafb745ea11db1479045d3534942a28
Berkanktk <berkank@live.dk>
2023-10-20 19:08:39 +0200

    Temporary page for rec-system

--- src/routes/recommendation/+page.svelte
+++ src/routes/recommendation/+page.svelte
@@ -1,5 +1,6 @@
-<script lang="ts">
-    import type { PageData } from './$types';
-    
-    export let data: PageData;
-</script>
+<div class="h-screen flex flex-col text-center items-center justify-center">
+    <h1 class="text-2xl">Implemented in the future</h1>
+    <button class="btn font-bold py-2 px-4 rounded mt-4">
+        <a href="/">Go back</a>
+    </button>
+</div>
