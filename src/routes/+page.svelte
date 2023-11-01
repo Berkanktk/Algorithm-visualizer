ce3349fba44c008d600944b02085309a031aa90f
Berkanktk <berkank@live.dk>
2023-11-01 15:24:04 +0100

    Deploying to github pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,11 @@
+<script>
+      import { base } from '$app/paths';
+</script>
+
 <div class="flex flex-col mx-auto items-center justify-center h-screen">
-    <a href="/sorting">Sorting</a>
-    <a href="/searching">Searching</a>
-    <a href="/pathfinding">Pathfinding</a>
-    <a href="/data-structures">Data Structures</a>
-    <a href="/recommendation">Recommendation</a>
+    <a href="{base}/sorting">Sorting</a>
+    <a href="{base}/searching">Searching</a>
+    <a href="{base}/pathfinding">Pathfinding</a>
+    <a href="{base}/data-structures">Data Structures</a>
+    <a href="{base}/recommendation">Recommendation</a>
 </div>
