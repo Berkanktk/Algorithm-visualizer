3d008c09f3cfccc5465ef65b73cdeecbf6e4141a
Berkanktk <berkank@live.dk>
2023-11-01 05:34:54 +0100

    Further refactoring

---
+++ src/lib/components/MazeGrid.svelte
@@ -0,0 +1,74 @@
+<script lang="ts">
+    export let grid: App.Cell[][];
+    export let isFinished: boolean;
+    export let isStart: (cell: App.Cell) => boolean;
+    export let isEnd: (cell: App.Cell) => boolean;
+</script>
+
+<div class="maze">
+    {#each grid as row}
+        {#each row as cell}
+            <div
+                class="cell {cell.walls[0] ? '' : 'no-top'} {cell.walls[1]
+                    ? ''
+                    : 'no-right'} {cell.walls[2] ? '' : 'no-bottom'} {cell
+                    .walls[3]
+                    ? ''
+                    : 'no-left'} {isStart(cell) && isFinished
+                    ? 'start'
+                    : ''} {isEnd(cell) && isFinished
+                    ? 'end'
+                    : ''} {cell.searched ? 'searched' : ''} {cell.lastChecked
+                    ? 'last-checked'
+                    : ''}"
+            />
+        {/each}
+    {/each}
+</div>
+
+<style>
+    .maze {
+        display: grid;
+        grid-template-columns: repeat(20, 20px);
+    }
+
+    .cell {
+        width: 20px;
+        height: 20px;
+        border-style: solid;
+        border-width: 1px;
+        border-color: white;
+    }
+
+    .no-top {
+        border-top: none;
+    }
+
+    .no-right {
+        border-right: none;
+    }
+
+    .no-bottom {
+        border-bottom: none;
+    }
+
+    .no-left {
+        border-left: none;
+    }
+
+    .start:nth-child(1) {
+        background-color: green;
+    }
+
+    .end:nth-child(400) {
+        background-color: red;
+    }
+
+    .searched {
+        background-color: blue;
+    }
+
+    .last-checked {
+        background-color: lightcoral;
+    }
+</style>
--- src/routes/pathfinding/+page.svelte
+++ src/routes/pathfinding/+page.svelte
@@ -2,6 +2,7 @@
     import { clearGrid } from "$lib/helpers";
     import { generateMaze } from "$lib/algorithms/maze-generators/DFS";
     import { aStar } from "$lib/algorithms/pathfinding/astar";
+    import MazeGrid from "$lib/components/MazeGrid.svelte";
 
     const rows: number = 20;
     const cols: number = 20;
@@ -84,26 +85,7 @@
         </div>
     </div>
 
-    <div class="maze mt-8">
-        {#each grid as row}
-            {#each row as cell}
-                <div
-                    class="cell {cell.walls[0] ? '' : 'no-top'} {cell.walls[1]
-                        ? ''
-                        : 'no-right'} {cell.walls[2] ? '' : 'no-bottom'} {cell
-                        .walls[3]
-                        ? ''
-                        : 'no-left'} {isStart(cell) && isFinished
-                        ? 'start'
-                        : ''} {isEnd(cell) && isFinished
-                        ? 'end'
-                        : ''} {cell.searched
-                        ? 'searched'
-                        : ''} {cell.lastChecked ? 'last-checked' : ''}"
-                />
-            {/each}
-        {/each}
-    </div>
+    <MazeGrid {grid} {isFinished} {isStart} {isEnd} />
 
     <div class="mt-8">
         <button class="btn btn-secondary" on:click={clearGrid}
@@ -111,50 +93,3 @@
         >
     </div>
 </div>
-
-<style>
-    .maze {
-        display: grid;
-        grid-template-columns: repeat(20, 20px);
-    }
-
-    .cell {
-        width: 20px;
-        height: 20px;
-        border-style: solid;
-        border-width: 1px;
-        border-color: white;
-    }
-
-    .no-top {
-        border-top: none;
-    }
-
-    .no-right {
-        border-right: none;
-    }
-
-    .no-bottom {
-        border-bottom: none;
-    }
-
-    .no-left {
-        border-left: none;
-    }
-
-    .start:nth-child(1) {
-        background-color: green;
-    }
-
-    .end:nth-child(400) {
-        background-color: red;
-    }
-
-    .searched {
-        background-color: blue;
-    }
-
-    .last-checked {
-        background-color: lightcoral;
-    }
-</style>
