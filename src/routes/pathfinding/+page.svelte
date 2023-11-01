9819731f7d8b6816a0155a4ff304d1331eb93325
Berkanktk <berkank@live.dk>
2023-11-01 03:48:10 +0100

    Added pathfinding with a*

--- src/routes/pathfinding/+page.svelte
+++ src/routes/pathfinding/+page.svelte
@@ -1,8 +1,17 @@
 <script lang="ts">
     type Cell = {
         visited: boolean;
+        searched: boolean; 
         walls: boolean[];
         position: [number, number];
+    };
+
+    type Node = {
+        cell: Cell;
+        parent: Node | null;
+        g: number;
+        h: number;
+        f: number;
     };
 
     const rows: number = 20;
@@ -11,6 +20,7 @@
     let stack: Cell[] = [];
     let disabled: boolean = false;
     let isFinished: boolean = false;
+    let isInstant: boolean = false;
 
     // Initialize grid
     for (let rowIndex = 0; rowIndex < rows; rowIndex++) {
@@ -18,11 +28,16 @@
         for (let colIndex = 0; colIndex < cols; colIndex++) {
             row.push({
                 visited: false,
+                searched: false, // Initialize to false
                 walls: [true, true, true, true],
                 position: [rowIndex, colIndex],
             });
         }
         grid.push(row);
+    }
+
+    function clearGrid(): void {
+        document.location.reload();
     }
 
     function isStart(cell: Cell): boolean {
@@ -54,7 +69,9 @@
                 removeWalls(current, next);
                 stack.push(current);
                 current = next;
-                // await delay(50);
+                if (!isInstant) {
+                    await delay(50);
+                }
                 grid = [...grid];
             } else if (stack.length > 0) {
                 current = stack.pop()!;
@@ -121,23 +138,167 @@
             b.walls[0] = false;
         }
     }
+
+    async function solveMaze(): Promise<void> {
+        // Initialize the open and closed list
+        let openList: Node[] = [];
+        let closedList: Set<Cell> = new Set();
+
+        // Heuristic function (Manhattan distance)
+        const heuristic = (cell: Cell): number => {
+            return (
+                Math.abs(rows - 1 - cell.position[0]) +
+                Math.abs(cols - 1 - cell.position[1])
+            );
+        };
+
+        // Create start node and goal node
+        let startNode: Node = {
+            cell: grid[0][0],
+            parent: null,
+            g: 0,
+            h: heuristic(grid[0][0]),
+            f: heuristic(grid[0][0]),
+        };
+        let goalNode: Cell = grid[rows - 1][cols - 1];
+
+        // Add the start node
+        openList.push(startNode);
+
+        // Loop until you find the end
+        while (openList.length > 0) {
+            // Get the current node
+            let currentNode = openList.reduce((a, b) => (a.f < b.f ? a : b));
+            openList = openList.filter((n) => n !== currentNode);
+            closedList.add(currentNode.cell);
+            currentNode.cell.searched = true; // Mark as searched
+
+            // Found the goal
+            if (currentNode.cell === goalNode) {
+                // Retrace path back to start
+                let pathNode: Node | null = currentNode;
+                while (pathNode != null) {
+                    pathNode.cell.visited = true; // Mark path cell as visited
+                    pathNode = pathNode.parent;
+                }
+                grid = [...grid];
+                return;
+            }
+
+            // Generate children
+            let children: Node[] = [];
+            for (let [dy, dx] of [
+                [-1, 0],
+                [0, 1],
+                [1, 0],
+                [0, -1],
+            ]) {
+                let newRow = currentNode.cell.position[0] + dy;
+                let newCol = currentNode.cell.position[1] + dx;
+
+                // Make sure within range
+                if (
+                    newRow < 0 ||
+                    newRow >= rows ||
+                    newCol < 0 ||
+                    newCol >= cols
+                ) {
+                    continue;
+                }
+
+                // Make sure walkable terrain
+                let neighborCell = grid[newRow][newCol];
+                if (
+                    (currentNode.cell.walls[0] && dy === -1) || 
+                    (currentNode.cell.walls[1] && dx === 1) || 
+                    (currentNode.cell.walls[2] && dy === 1) || 
+                    (currentNode.cell.walls[3] && dx === -1) 
+                ) {
+                    continue;
+                }
+
+                // Create new node
+                let newNode: Node = {
+                    cell: neighborCell,
+                    parent: currentNode,
+                    g: currentNode.g + 1,
+                    h: heuristic(neighborCell),
+                    f: currentNode.g + 1 + heuristic(neighborCell),
+                };
+
+                children.push(newNode);
+            }
+
+            // Loop through children
+            for (let child of children) {
+                // Child is on the closed list
+                if (closedList.has(child.cell)) {
+                    continue;
+                }
+
+                // Add the child to the open list
+                openList.push(child);
+            }
+
+            if (!isInstant) {
+                await delay(50);
+            }
+            grid = [...grid];
+        }
+    }
 </script>
 
 <div class="justify-center align-middle mx-auto text-center mt-16">
+    <h1 class="text-4xl font-bold mb-8">Pathfinding Visualizer</h1>
+
     <div>
-        <button class="btn btn-primary" on:click={generateMaze} {disabled}
-            >Generate Maze</button
+        <button
+            class="btn btn-primary"
+            on:click={generateMaze}
+            disabled={isFinished}>Generate Maze</button
         >
+        <button
+            class="btn btn-primary"
+            on:click={solveMaze}
+            disabled={!isFinished}>Solve Maze</button
+        >
+
+        <div class="form-control mt-8">
+            <label class="label cursor-pointer justify-center">
+                <span class="label-text">No Animation</span>
+                <input
+                name="instant"
+                type="checkbox"
+                bind:checked={isInstant}
+                class="toggle toggle-accent ml-4"
+                />
+            </label>
+        </div>
     </div>
 
     <div class="maze mt-8">
         {#each grid as row}
             {#each row as cell}
                 <div
-                class="cell {cell.walls[0] ? '' : 'no-top'} {cell.walls[1] ? '' : 'no-right'} {cell.walls[2] ? '' : 'no-bottom'} {cell.walls[3] ? '' : 'no-left'} {isStart(cell) && isFinished ? 'start' : ''} {isEnd(cell) && isFinished? 'end' : ''}"
+                    class="cell {cell.walls[0] ? '' : 'no-top'} {cell.walls[1]
+                        ? ''
+                        : 'no-right'} {cell.walls[2] ? '' : 'no-bottom'} {cell
+                        .walls[3]
+                        ? ''
+                        : 'no-left'} {isStart(cell) && isFinished
+                        ? 'start'
+                        : ''} {isEnd(cell) && isFinished
+                        ? 'end'
+                        : ''} {cell.searched ? 'searched' : ''}"
                 />
             {/each}
         {/each}
+    </div>
+
+    <div class="mt-8">
+        <button class="btn btn-secondary" on:click={clearGrid}
+            >Reset Maze</button
+        >
     </div>
 </div>
 
@@ -177,5 +338,9 @@
 
     .end:nth-child(400) {
         background-color: red;
+    }
+
+    .searched {
+        background-color: lightcoral;
     }
 </style>
